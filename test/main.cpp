8a05620e2488b6a7d43b2aae74caf41ca718fabe
Tony-Wang-crcHK <wyq18134091031@gmail.com>
2024-09-27 16:02:22 +0800

    ros demo

--- test/main.cpp
+++ test/main.cpp
@@ -1,86 +1,51 @@
 #include "sdk/api.h"
 #include <iostream>
 #include <unistd.h>
-#include "sdk/BuffShared.h"
-#include "sdk/BuffShared.cpp"
 #include <sys/time.h>
+
+#include <mutex>
+#include <deque>
+#include <thread>
+#include <unistd.h>
+#include <iostream>
+#include <csignal>
+
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
 
 using namespace std;
 
-void test_buff_shared()
+void signalHandler(int signal)
 {
-    // 创建buffshare变量
-    CBuffShared mBuff;
-    void *buff[4];
-    char mCharBuff[4][20];
-    void *p = NULL;
-    int numAdd = 0, numGet = 0;
-
-    for(int i = 0; i< 4 ; i++)
+    if (signal == SIGINT)
     {
-        buff[i] = (void *)mCharBuff[i];
+        ROS_INFO("Caught SIGINT, shutting down node.");
+        ros::shutdown();
+        exit(0);
     }
-
-    // 初始化缓冲区
-    mBuff.Init(buff , 4);
-
-    for( int i = 0; i<20; i++)
-    {
-        for(int j=0; j<2;j++)
-        {
-            if(NULL == mBuff.GetBuff(CBuffShared::WRITING))
-            {
-                printf("getbuff failed. \n\r");
-                break;
-            }
-            numAdd++;
-        }
-
-        p = mBuff.GetBuff(CBuffShared::READING);
-        if(p == NULL)
-        {
-            printf("get reading buff failed!\n\r");
-            if(mBuff.IsFull())
-            {
-                printf("is full \n\r");
-            }
-        }else{
-            numGet++ ;
-        }
-        // if(NULL == mBuff.GetBuff(CBuffShared::WRITING))
-        // {
-        //     cout << "get writing buff failed." << endl;
-
-        // }
-        
-        // cout << "add i:" << i ;
-        
-        // if( mBuff.IsFull())
-        // {
-        //     int j = 0;
-        //     cout << " FULL" << endl;
-        //     while(mBuff.IsFull())
-        //     {
-                
-        //         mBuff.GetBuff(CBuffShared::READING);
-        //         cout << "GetBuff times: " << j++ <<endl;
-        //     }
-        // }else {
-        //     cout << " no full " << endl;
-        // }
-    }
-    printf("addNum:%d    getNum:%d\n\r", numAdd , numGet);
 }
 
-void test()
+int main(int argc, char **argv)
 {
+    // 初始化ROS节点
+    ros::init(argc, argv, "test_sensor_array");
+    ros::NodeHandle nh;
+
+    // 创建一个发布者，发布到"/depth_map"话题，消息类型为sensor_msgs/Image
+    ros::Publisher depth_pub = nh.advertise<sensor_msgs::Image>("/depth_map", 1);
+
+    std::signal(SIGINT, signalHandler);
+
+    // 初始化设备ID连接时间等变量， 并初始化SDK
     int DevID = 0;
-    struct timeval startTime , endTime;
     STRC_IMG_ALL *pImg;
-    int mNumFrame = 0;
     char *pInfo = NULL;
+
     api_init();
-    
+
     cout << "SDK VER:" << api_get_sdk_ver() << endl;
 
     //connect
@@ -89,56 +54,74 @@ void test()
     if(DevID < 0)
     {
         cout << "api_connect failed." << endl;
-        goto END;
+        return -1;
     }
     sleep(1);
     pInfo = api_get_dev_ver(DevID);
-    
+
     if(pInfo == NULL)
     {
         cout << "get Sdk version failed." << endl;
-    }else{
-        cout << "SDK VER:" << pInfo << endl; 
-
+    }
+    else
+    {
+        cout << "SDK VER:" << pInfo << endl;
     }
 
     pInfo = api_get_intrinsic_parameters(DevID);
-    
+
     if(pInfo == NULL)
     {
         cout << "get lens failed." << endl;
-    }else{
-        cout << "lens info:" << pInfo << endl; 
-
     }
-    
-    while((pImg = api_get_img(DevID) ) == NULL);
-    gettimeofday(&startTime , NULL);
-    while(mNumFrame < 60)
+    else
     {
-        if( (pImg = api_get_img(DevID)) != NULL)
-        {
-            mNumFrame++;
-        }
+        cout << "lens info:" << pInfo << endl;
+
     }
-    gettimeofday(&endTime , NULL);
-    cout << "mNumFrame:" << mNumFrame << endl;
-    cout << "sec:" << endTime.tv_sec - startTime.tv_sec << endl;
-    cout << "usec:" << endTime.tv_usec - startTime.tv_usec << endl;
 
-    cout << "fps: " << (int)(60/(endTime.tv_sec - startTime.tv_sec)) << endl;
+    ros::Rate loop_rate(10);
 
+    // 定义图像的宽度和高度
+    int width = 640;
+    int height = 480;
 
-    sleep(3);
+    // 创建一个640x480的深度图，数据类型为CV_32F（32位浮点数）
+    cv::Mat depthMap(height, width, CV_32F, cv::Scalar(0));
 
-END:
-    api_exit();    
-}
+    // 创建一个窗口
+    const char* windowName = "Depth Map";
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
 
-int main()
-{
-    int ret = 0;
-    // test_buff_shared();
-    test();
-    return ret;
+    while(ros::ok())
+    {
+        pImg = api_get_img(DevID);
+
+        if(pImg != NULL)
+        {
+            // 给每个像素赋值，这里以赋值为它的索引位置为例
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    float& pixel = depthMap.at<float>(i, j);
+                    pixel = static_cast<float>(pImg -> img_depth.data[i * width + j]);
+                }
+            }
+
+            // 转换OpenCV图像到ROS图像消息
+            sensor_msgs::ImagePtr depth_msg = cv_bridge::CvImage(std_msgs::Header(), "32FC1", depthMap).toImageMsg();
+
+            // 发布深度图
+            depth_pub.publish(depth_msg);
+        }
+
+        loop_rate.sleep();
+    }
+
+    api_disconnect(DevID);
+
+    api_exit();
+
+    return 0;
 }
